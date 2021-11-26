8e4a2fe5538f87203f55dfa54ed55b8520f35ca0
Sedatyf <gaxel@hotmail.fr>
2021-11-26 16:06:25 +0100

    added information in bot's status

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,11 @@ module.exports = {
     once: true,
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
+        client.user.setActivity(
+            'Type / to have different options. I also react on certain messages',
+            {
+                type: 'PLAYING',
+            }
+        );
     },
 };
