0461ad1d35f71aa987b13b5d2ec0103ee790d881
Sedatyf <gaxel@hotmail.fr>
2021-12-02 16:30:16 +0100

    add wheel slash command

---
+++ commands/wheel.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const utils = require('../tools/utils.js');
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('wheel')
+        .setDescription('WIP')
+        .addStringOption(option =>
+            option
+                .setName('elements')
+                .setDescription(
+                    "Les éléments à mettre dans la roue. Séparer les éléments d'un espace"
+                )
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const originalString = interaction.options.data[0].value;
+        const choicesArray = originalString.split(' ');
+        rand = utils.generateRandomForArray(choicesArray);
+        await interaction.reply('Je fais tourner la roue');
+        await interaction.channel.send(
+            'https://tenor.com/view/the-goon-prize-wheel-wheel-of-excuses-excuses-no-gif-14618546'
+        );
+        await delay(5000);
+        await interaction.channel.send(`Et c'est tombé sur **${choicesArray[rand]}**`);
+    },
+};
