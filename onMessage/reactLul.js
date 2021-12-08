d129994d38b89a8166d204223f143d784beeb3ca
Sedatyf <gaxel@hotmail.fr>
2021-12-08 17:22:09 +0100

    fix delay to reactLUL

--- onMessage/reactLul.js
+++ onMessage/reactLul.js
@@ -4,13 +4,14 @@ const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 module.exports = {
     reactLul: async function (client, message) {
         const userMessage = message.cleanContent.toLowerCase();
-        const rand = utils.getRandomIntInclusive(2, 10) * 1000;
         const lul = client.emojis.cache.find(emoji => emoji.name === 'lul');
         if (userMessage.includes('mdr') || userMessage.includes('lol')) {
+            let rand = utils.getRandomIntInclusive(2, 10) * 1000;
             await delay(rand);
             await message.react(lul);
         }
         if (userMessage.includes('<:lul:375955064931745792>')) {
+            let rand = utils.getRandomIntInclusive(2, 10) * 1000;
             await delay(rand);
             await message.react(lul);
         }
