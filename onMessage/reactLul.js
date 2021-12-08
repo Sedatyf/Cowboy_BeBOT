aac717b0bb23d579a0d5a56dfa964098ce7b741e
Sedatyf <gaxel@hotmail.fr>
2021-12-08 14:40:28 +0100

    add delay to ReactLuL

--- onMessage/reactLul.js
+++ onMessage/reactLul.js
@@ -1,12 +1,18 @@
+const utils = require('../tools/utils.js');
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 module.exports = {
-    reactLul: function (client, message) {
+    reactLul: async function (client, message) {
         const userMessage = message.cleanContent.toLowerCase();
+        const rand = utils.getRandomIntInclusive(2, 10) * 1000;
         const lul = client.emojis.cache.find(emoji => emoji.name === 'lul');
         if (userMessage.includes('mdr') || userMessage.includes('lol')) {
-            message.react(lul);
+            await delay(rand);
+            await message.react(lul);
         }
         if (userMessage.includes('<:lul:375955064931745792>')) {
-            message.react(lul);
+            await delay(rand);
+            await message.react(lul);
         }
     },
 };
