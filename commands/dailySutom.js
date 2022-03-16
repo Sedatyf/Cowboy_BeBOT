3ec97268eb360a5a045797d2c7d7909997451eab
Sedatyf <gaxel@hotmail.fr>
2022-03-16 14:12:03 +0100

    sort dailySutom score

--- commands/dailySutom.js
+++ commands/dailySutom.js
@@ -3,6 +3,7 @@ const path = require('path');
 const utils = require('../tools/utils.js');
 
 const SUTOM_JSON_PATH = path.join(__dirname, '..', 'data', 'sutomScore.json');
+const keyValue = [];
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,21 +13,29 @@ module.exports = {
         const sutomJson = require('../data/sutomScore.json');
         const currentSutom = utils.getCurrentSutom(SUTOM_JSON_PATH).toString();
 
-        let message = '';
         for (const [user, value] of Object.entries(sutomJson.users)) {
             if (!(currentSutom in value.sutomScore)) continue;
-            message += `${user} : `;
             for (const [score, scoreValue] of Object.entries(value['sutomScore'])) {
                 if (score === currentSutom) {
-                    message += scoreValue + '\n';
+                    keyValue.push([user, scoreValue]);
                 }
             }
         }
-        if (message === '') {
+
+        keyValue.sort(function (a, b) {
+            return a[1] - b[1];
+        });
+
+        if (keyValue.length === 0) {
             await interaction.reply("Je n'ai enregistré aucun score pour aujourd'hui");
         } else {
+            let message = '';
+            console.log(keyValue);
+            for (let i = 0; i < keyValue.length; i++) {
+                message += `${keyValue[i][0]} : ${keyValue[i][1]}\r\n`;
+            }
             await interaction.reply(
-                "Voici les scores que j'ai enregistré pour le Sutom du jour par ordre d'arrivée"
+                "Voici les scores que j'ai enregistré pour le Sutom du jour trié par score"
             );
             await interaction.channel.send(message);
         }
