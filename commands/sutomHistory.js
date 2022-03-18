81e1db96b184ffc78969efed1232e0cb85f04dc2
Sedatyf <gaxel@hotmail.fr>
2022-03-18 14:41:17 +0100

    add sutomHistory

---
+++ commands/sutomHistory.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+let keyValue = [];
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('historysutom')
+        .setDescription("Affiche les cinq derniers scores d'une personne")
+        .addStringOption(option =>
+            option
+                .setName('player')
+                .setDescription('Le nom discord de la personne')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const sutomJson = require('../data/sutomScore.json');
+        const user = interaction.options.data[0].value.toLowerCase();
+
+        if (!(user in sutomJson.users)) {
+            await interaction.reply(
+                `Je n'ai pas trouvé la personne ${interaction.options.data[0].value}`
+            );
+        }
+        for (const sutomScore in sutomJson['users'][user]['sutomScore']) {
+            keyValue.push([sutomScore, sutomJson['users'][user]['sutomScore'][sutomScore]]);
+        }
+        keyValue = keyValue.slice(Math.max(keyValue.length - 5, 0));
+        let message = '';
+        for (let i = 0; i < keyValue.length; i++) {
+            message += `**${keyValue[i][0]}** : ${keyValue[i][1]}\r\n`;
+        }
+        await interaction.reply(
+            `Voici les 5 derniers scores Sutom enregistrés pour **${user}**\r\n${message}`
+        );
+    },
+};
