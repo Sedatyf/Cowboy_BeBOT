dbdf1a07e37fd5c8f364b21b68c4e3a20120caef
Sedatyf <gaxel@hotmail.fr>
2022-04-07 15:13:49 +0200

    refactore 81212

---
+++ archive/commands/81212.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const utils = require('../tools/utils.js');
+const constant = require('../data/81212.json');
+
+/* Ancien système 81212 avec match dedans. Mais personne ne l'utilise */
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('81212')
+        .setDescription('📱 Envoie zicmu, pecho ou match pour seulement 2€ + prix du SMS !')
+        .addStringOption(option =>
+            option
+                .setName('service')
+                .setDescription('Le service que vous voulez joindre')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const service = interaction.options.data[0].value;
+        switch (service.toLowerCase()) {
+            case 'zicmu':
+                const randZik = utils.generateRandomForArray(constant.zikmu);
+                interaction.reply(
+                    `Voici un super son à mettre dans ton téléphone. Tes amis seront tous jaloux !\n${constant.zikmu[randZik]}`
+                );
+                break;
+            case 'pecho':
+                const randPecho = utils.generateRandomForArray(constant.pecho);
+                interaction.reply(
+                    `Voici un message que tu peux envoyer pour pécho à coup sur !\n**${constant.pecho[randPecho]}**`
+                );
+                break;
+            default:
+                if (interaction.options.data[0].value.includes('match')) {
+                    const names = interaction.options.data[0].value.split(' ');
+                    if (names.length < 3) {
+                        interaction.reply(
+                            `T'as oublié de préciser les noms des gens avec qui tu veux savoir la compatibilité !
+Tu seras quand même facturé 2€ + prix du SMS
+Envoie match [nom1] [nom2] au 8 12 12`
+                        );
+                        break;
+                    }
+
+                    interaction.reply(
+                        `${names[1]} et ${
+                            names[2]
+                        } vous avez une compabilité de ${utils.getRandomIntInclusive(0, 100)}%`
+                    );
+                    break;
+                }
+                interaction.reply(
+                    "Ce service n'existe pas ! Tu seras quand même facturé 2€ + prix du SMS"
+                );
+                break;
+        }
+    },
+};
--- commands/81212.js
+++ commands/81212.js
@@ -13,41 +13,26 @@ module.exports = {
                 .setName('service')
                 .setDescription('Le service que vous voulez joindre')
                 .setRequired(true)
+                .addChoice('Zicmu', 'zicmu')
+                .addChoice('Pecho', 'pecho')
         ),
     async execute(interaction) {
-        const service = interaction.options.data[0].value;
-        switch (service.toLowerCase()) {
-            case 'zicmu':
+        switch (interaction.options.getString('service')) {
+            case 'zicmu': {
                 const randZik = utils.generateRandomForArray(constant.zikmu);
                 interaction.reply(
                     `Voici un super son à mettre dans ton téléphone. Tes amis seront tous jaloux !\n${constant.zikmu[randZik]}`
                 );
                 break;
-            case 'pecho':
+            }
+            case 'pecho': {
                 const randPecho = utils.generateRandomForArray(constant.pecho);
                 interaction.reply(
                     `Voici un message que tu peux envoyer pour pécho à coup sur !\n**${constant.pecho[randPecho]}**`
                 );
                 break;
+            }
             default:
-                if (interaction.options.data[0].value.includes('match')) {
-                    const names = interaction.options.data[0].value.split(' ');
-                    if (names.length < 3) {
-                        interaction.reply(
-                            `T'as oublié de préciser les noms des gens avec qui tu veux savoir la compatibilité !
-Tu seras quand même facturé 2€ + prix du SMS
-Envoie match [nom1] [nom2] au 8 12 12`
-                        );
-                        break;
-                    }
-
-                    interaction.reply(
-                        `${names[1]} et ${
-                            names[2]
-                        } vous avez une compabilité de ${utils.getRandomIntInclusive(0, 100)}%`
-                    );
-                    break;
-                }
                 interaction.reply(
                     "Ce service n'existe pas ! Tu seras quand même facturé 2€ + prix du SMS"
                 );
@@ -55,8 +40,3 @@ Envoie match [nom1] [nom2] au 8 12 12`
         }
     },
 };
-
-/* TODO: 81212 match: un calculateur de compatibilité
-          81212 musique
-          81212 gif?
-*/
