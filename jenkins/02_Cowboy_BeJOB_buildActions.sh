bb252f8a7fda42c07af25340d052cc732d11b781
Sedatyf <gaxel@hotmail.fr>
2022-04-27 19:16:12 +0200

    modify buildActions

--- jenkins/02_Cowboy_BeJOB_buildActions.sh
+++ jenkins/02_Cowboy_BeJOB_buildActions.sh
@@ -1,4 +1,7 @@
-sudo kill $(ps ax | grep 'node' | grep -v grep | awk '{print $1}')
+if pgrep -x "node" > /dev/null
+then
+    sudo kill $(ps ax | grep 'node' | grep -v grep | awk '{print $1}')
+fi
 cp /usr/share/.env /var/lib/jenkins/workspace/02_Cowboy_BeJOB/.env
 npm install
 cp -a /usr/share/CBB_data/. /var/lib/jenkins/workspace/02_Cowboy_BeJOB/data
