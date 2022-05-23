10e36b1f947be88ab526674756904000a9300b20
Sedatyf <gaxel@hotmail.fr>
2022-05-23 11:41:52 +0200

    add test get random int inclusive

--- test/test_utils.js
+++ test/test_utils.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+const expect = require('chai').expect;
 const utils = require('../tools/utils');
 const path = require('path');
 
@@ -6,6 +6,13 @@ const SUTOM_JSON_PATH = path.join(__dirname, 'testData', 'test_dailyScore.json')
 
 describe('Verify Get current sutom data', () => {
     it('should return 135', () => {
-        assert.equal(utils.getCurrentSutom(SUTOM_JSON_PATH), 135);
+        expect(utils.getCurrentSutom(SUTOM_JSON_PATH)).to.equal(135);
+    });
+});
+
+describe('Verify Get Random int inclusive', () => {
+    it('should be lower than max and higher than min', () => {
+        expect(utils.getRandomIntInclusive(0, 10)).to.be.above(0);
+        expect(utils.getRandomIntInclusive(0, 10)).to.be.below(10);
     });
 });
