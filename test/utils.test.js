fa45e5f26769371b4b0d56d4a5fc1b3ab15504f9
Sedatyf <gaxel@hotmail.fr>
2022-05-23 14:53:55 +0200

    change mocha to jest

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,4 +1,3 @@
-const expect = require('chai').expect;
 const utils = require('../tools/utils');
 const path = require('path');
 
@@ -6,13 +5,13 @@ const SUTOM_JSON_PATH = path.join(__dirname, 'testData', 'test_dailyScore.json')
 
 describe('Verify Get current sutom data', () => {
     it('should return 135', () => {
-        expect(utils.getCurrentSutom(SUTOM_JSON_PATH)).to.equal(135);
+        expect(utils.getCurrentSutom(SUTOM_JSON_PATH)).toBe(135);
     });
 });
 
 describe('Verify Get Random int inclusive', () => {
     it('should be lower than max and higher than min', () => {
-        expect(utils.getRandomIntInclusive(0, 10)).to.be.above(0);
-        expect(utils.getRandomIntInclusive(0, 10)).to.be.below(10);
+        expect(utils.getRandomIntInclusive(0, 10)).toBeGreaterThanOrEqual(0);
+        expect(utils.getRandomIntInclusive(0, 10)).toBeLessThanOrEqual(10);
     });
 });
