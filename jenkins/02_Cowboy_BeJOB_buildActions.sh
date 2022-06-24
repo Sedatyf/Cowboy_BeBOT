2fdabf0056af83ed095a1fda7be4284867bd96a3
Sedatyf <gaxel@hotmail.fr>
2022-06-24 15:03:28 +0200

    update jenkins actions 🤵

--- jenkins/02_Cowboy_BeJOB_buildActions.sh
+++ jenkins/02_Cowboy_BeJOB_buildActions.sh
@@ -1,8 +1,8 @@
 if pgrep -x "node" > /dev/null
 then
-    pm2 stop index.js
+    pm2 stop cbb
 fi
 cp /usr/share/.env /var/lib/jenkins/workspace/02_Cowboy_BeJOB/.env
 npm install
 cp -a /usr/share/CBB_data/. /var/lib/jenkins/workspace/02_Cowboy_BeJOB/data
-BUILD_ID=dontKillMe pm2 start index.js --time
+BUILD_ID=dontKillMe pm2 start cbb --time
