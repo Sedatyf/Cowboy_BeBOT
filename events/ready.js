a057443bdb641458231b9c59fcafc2168e8ec287
Sedatyf <gaxel@hotmail.fr>
2022-07-30 14:31:11 +0200

    change on ready event to match v14 ⬆

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,18 @@
+const { ActivityType } = require('discord.js');
+
 module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
-        client.user.setActivity(
-            'Type / to have different options. I also react on certain messages',
-            {
-                type: 'PLAYING',
-            }
-        );
+        client.user.setPresence({
+            activities: [
+                {
+                    name: `Type / to have different options. I also react on certain messages`,
+                    type: ActivityType.Playing,
+                },
+            ],
+            status: 'online',
+        });
     },
 };
